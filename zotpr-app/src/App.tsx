24079ede053f8c7e4d6950fa700cff69f6bfab74
Kelsey <kbeau@uci.edu>
2025-05-14 21:10:29 -0700

    switched to typescript

---
+++ zotpr-app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-2xl">Welcome to ZotPR!</h1>
+      <h2 className="text-xl">{"<(^_^)>"}</h2>
+    </div>
+  )
+}
+
+export default App
