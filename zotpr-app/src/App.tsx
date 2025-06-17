7cc918a4bc5c25c8c1731eccb3aafcd253f32109
Kelsey <kbeau@uci.edu>
2025-06-16 21:42:14 -0700

    fixed tailwind import, base home layout

--- zotpr-app/src/App.tsx
+++ zotpr-app/src/App.tsx
@@ -1,10 +1,21 @@
+import Navbar from './components/navbar';
+
 import './App.css'
 
 function App() {
   return (
-    <div>
-      <h1 className="text-2xl">Welcome to ZotPR!</h1>
-      <h2 className="text-xl">{"<(^_^)>"}</h2>
+    <div className="h-screen flex flex-col items-center text-white"
+         style={{ backgroundImage: 'var(--background-image)', opacity: 0.9,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat'
+          }}>
+    <div className="absolute inset-0 bg-black opacity-30"></div>
+      <Navbar />
+      <div>
+          <h1 className="text-2xl text-center py-4">Welcome to ZotPR!</h1>
+          <h2 className="text-xl text-center">{"<(^_^)>"}</h2>
+      </div>
     </div>
   )
 }
